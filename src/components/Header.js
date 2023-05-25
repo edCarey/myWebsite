884d5ddfb1bc6195d54a74c5acea7392f80b8518
edCarey <ecarey23@gmail.com>
2023-05-25 15:45:31 +0100

    amended issues

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,33 +3,26 @@ import { AppBar, Toolbar, Typography, Button } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { Link as RouterLink } from 'react-router-dom';
 
-function scrollToTop() {
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth',
-  });
-}
-
 const Header = () => {
   return (
     <AppBar position="sticky" sx={{ backgroundColor: '#F6F5F4'}}>
       <Toolbar>
         <RouterLink to="/" style={{ display: 'flex', alignItems: 'center', textDecoration: 'none' }}>
-          <Image>
-            
-          </Image>
+          <Typography color="primary" variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Ed Carey
+          </Typography>
         </RouterLink>
         <div style={{ flexGrow: 1 }}></div>
-        <Button color="primary" onClick={scrollToTop} component={Link} to="/">
+        <Button color="primary" component={Link} to="/">
           Products
         </Button>
-        <Button color="primary" onClick={scrollToTop} component={Link} to="/about">
+        <Button color="primary" component={Link} to="/about">
           About
         </Button>
-        <Button color="primary" onClick={scrollToTop} component={Link} to="/blog">
+        <Button color="primary" component={Link} to="/blog">
           Blog
         </Button>
-        <Button color="primary" onClick={scrollToTop} component={Link} to="/cv">
+        <Button color="primary" component={Link} to="/cv">
           CV
         </Button>  
       </Toolbar>
