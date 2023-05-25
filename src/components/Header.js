60485bb47ea579f676a90d70f661cdac2abedeb9
edCarey <ecarey23@gmail.com>
2023-05-25 15:33:17 +0100

    added scroll to top to buttons in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,26 +3,33 @@ import { AppBar, Toolbar, Typography, Button } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { Link as RouterLink } from 'react-router-dom';
 
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+}
+
 const Header = () => {
   return (
     <AppBar position="sticky" sx={{ backgroundColor: '#F6F5F4'}}>
       <Toolbar>
         <RouterLink to="/" style={{ display: 'flex', alignItems: 'center', textDecoration: 'none' }}>
-          <Typography color="#464545" variant="h6" component="div">
-            Ed Carey
-          </Typography>
+          <Image>
+            
+          </Image>
         </RouterLink>
         <div style={{ flexGrow: 1 }}></div>
-        <Button color="primary" component={Link} to="/">
+        <Button color="primary" onClick={scrollToTop} component={Link} to="/">
           Products
         </Button>
-        <Button color="primary" component={Link} to="/about">
+        <Button color="primary" onClick={scrollToTop} component={Link} to="/about">
           About
         </Button>
-        <Button color="primary" component={Link} to="/blog">
+        <Button color="primary" onClick={scrollToTop} component={Link} to="/blog">
           Blog
         </Button>
-        <Button color="primary" component={Link} to="/cv">
+        <Button color="primary" onClick={scrollToTop} component={Link} to="/cv">
           CV
         </Button>  
       </Toolbar>
